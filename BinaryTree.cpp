cdf30000b8132766cc31ad6bbf03313f7a938b3c
Muhammad-Lutfi-RGB <muhammad.lutfi.ft24@mail.umy.ac.id>
2025-06-26 15:25:24 +0700

    step 3 insert

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -47,3 +47,7 @@ class BinaryTree {
 
         // step 2: Assign value to the data field of the new node
         newNode->info = x;
+
+        //step 3: Make the left and right child of the new node point to NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
