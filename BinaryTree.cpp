3e32126ccd80794618db5ee0943d23cbda55751e
Muhammad-Lutfi-RGB <muhammad.lutfi.ft24@mail.umy.ac.id>
2025-06-26 15:10:07 +0700

    membuat class dan constructor untuk binary tree dengan membuat root menunjuk ke NULL

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -13,3 +13,11 @@ class Node {
         rightchild = nullptr; // Initialize right child to null
     }
 };
+
+class BinaryTree {
+    public:
+    Node *ROOT;
+
+    BinaryTree () {
+        ROOT = nullptr; // Initialize ROOT to null
+    }
